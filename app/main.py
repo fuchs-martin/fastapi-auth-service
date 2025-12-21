d4769d214eb3ab184cfe17010ce00f6284bd4d8f
fuchs-martin <martinfuxxs@gmail.com>
2025-12-21 18:23:51 +0100

    chore(backend): update Main module

--- app/main.py
+++ app/main.py
@@ -4,16 +4,14 @@ from app.auth.router import router as auth_router
 from app.database import Base, engine
 from app import models
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title="FastAPI Auth Services")
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
 app.include_router(auth_router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
