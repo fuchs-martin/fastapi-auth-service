1e89932fce6688f1e22a56bbcfddc6921c57ea4f
fuchs-martin <martinfuxxs@gmail.com>
2025-12-21 18:13:31 +0100

    chore(backend): update Config module

--- app/config.py
+++ app/config.py
@@ -1,11 +1,18 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    # App
+    ENV: str
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    class Config:
-        env_file = ".env"
+    # Database
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(
+        env_files=".env",
+        extra="ignore",
+    )
 
 settings = Settings()
