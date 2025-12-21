98a1c14c21809a8fdf389707c2a1c7915e0b524f
fuchs-martin <martinfuxxs@gmail.com>
2025-12-21 18:22:16 +0100

    chore(backend): update Database module

--- app/database.py
+++ app/database.py
@@ -1,17 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./auth.db"
+from app.config import settings
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
 )
 
 class Base(DeclarativeBase):
     pass
 
-SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False)
 
 def get_db():
     db = SessionLocal()
